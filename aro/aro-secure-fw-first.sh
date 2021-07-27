3fa162faebe7c6a6dc4bace6bae644a9ad9f409e
Mark Whitby <m@markw.me>
2021-07-27 23:26:37 +0100

    ARO script to create firewall first

--- aro/aro-secure-fw-first.sh
+++ aro/aro-secure-fw-first.sh
@@ -3,41 +3,43 @@
 #
 # ARO Secure Private Cluster
 # Deploys ARO along with Azure Firewall and other security enhancements
+# This script differs from the aro-secure.sh script as it creates the firewall and rules prior to the ARO deployment
+#
+# This script is an experiment to see if enabling firewall rules before cluster deployment works.
 #
 # Get the pull secret from https://cloud.redhat.com/openshift/install/azure/aro-provisioned
 #
 # Set environment variables
 #
 LOCATION=westeurope
-RESOURCEGROUP=arosecure
-CLUSTER=arosecure
-virtualNetworkPrefix=10.152.0.0/21
-masterSubnetPrefix=10.152.0.0/23
-workerSubnetPrefix=10.152.2.0/23
-firewallSubnetPrefix=10.152.4.0/23
-jumpboxSubnetPrefix=10.152.6.0/23
+NAME=arosecurefw
+virtualNetworkPrefix=10.153.0.0/21
+masterSubnetPrefix=10.153.0.0/23
+workerSubnetPrefix=10.153.2.0/23
+firewallSubnetPrefix=10.153.4.0/23
+jumpboxSubnetPrefix=10.153.6.0/23
 
 #
 # Create resource group
 #
 az group create \
-  --name $RESOURCEGROUP \
+  --name $NAME \
   --location $LOCATION
 
 #
 # Create virtual network
 #
 az network vnet create \
-   --resource-group $RESOURCEGROUP \
-   --name aro-vnet \
+   --resource-group $NAME \
+   --name ${NAME}-vnet \
    --address-prefixes $virtualNetworkPrefix
 
 #
 # Subnet for master nodes
 #
 az network vnet subnet create \
-  --resource-group $RESOURCEGROUP \
-  --vnet-name aro-vnet \
+  --resource-group $NAME \
+  --vnet-name ${NAME}-vnet \
   --name master-subnet \
   --address-prefixes $masterSubnetPrefix \
   --service-endpoints Microsoft.ContainerRegistry
@@ -46,8 +48,8 @@ az network vnet subnet create \
 # Subnet for worker nodes
 #
 az network vnet subnet create \
-  --resource-group $RESOURCEGROUP \
-  --vnet-name aro-vnet \
+  --resource-group $NAME \
+  --vnet-name ${NAME}-vnet \
   --name worker-subnet \
   --address-prefixes $workerSubnetPrefix \
   --service-endpoints Microsoft.ContainerRegistry
@@ -57,16 +59,16 @@ az network vnet subnet create \
 #
 az network vnet subnet update \
   --name master-subnet \
-  --resource-group $RESOURCEGROUP \
-  --vnet-name aro-vnet \
+  --resource-group $NAME \
+  --vnet-name ${NAME}-vnet \
   --disable-private-link-service-network-policies true
 
 #
 # Subnet for firewall
 #
 az network vnet subnet create \
-  --resource-group $RESOURCEGROUP \
-  --vnet-name aro-vnet \
+  --resource-group $NAME \
+  --vnet-name ${NAME}-vnet \
   --name AzureFirewallSubnet \
   --address-prefixes $firewallSubnetPrefix
 
@@ -74,8 +76,8 @@ az network vnet subnet create \
 # Subnet for Jump Box VM
 #
 az network vnet subnet create \
-  --resource-group $RESOURCEGROUP \
-  --vnet-name aro-vnet \
+  --resource-group $NAME \
+  --vnet-name ${NAME}-vnet \
   --name jumpbox-subnet \
   --address-prefixes $jumpboxSubnetPrefix \
   --service-endpoints Microsoft.ContainerRegistry
@@ -86,32 +88,19 @@ az network vnet subnet create \
 VMUSERNAME=aroadmin
 
 az vm create --name ubuntu-jump \
-  --resource-group $RESOURCEGROUP \
+  --resource-group $NAME \
   --ssh-key-values ~/.ssh/id_rsa.pub \
   --admin-username $VMUSERNAME \
   --image UbuntuLTS \
   --subnet jumpbox-subnet \
   --public-ip-address jumphost-ip \
   --public-ip-sku Standard \
-  --vnet-name aro-vnet
-
-#
-# Create the cluster
-#
-az aro create \
-  --resource-group $RESOURCEGROUP \
-  --name $CLUSTER \
-  --vnet aro-vnet \
-  --master-subnet master-subnet \
-  --worker-subnet worker-subnet \
-  --apiserver-visibility Private \
-  --ingress-visibility Private \
-  --pull-secret @/mnt/c/Users/mtjw/Downloads/pull-secret.txt
+  --vnet-name ${NAME}-vnet
 
 #
 # Create a public IP address for the firewall
 #
-az network public-ip create -g $RESOURCEGROUP -n fw-ip --sku "Standard" --location $LOCATION
+az network public-ip create -g $NAME -n ${NAME}-ip --sku "Standard" --location $LOCATION
 
 #
 # Add / update the Azure Firewall extension for the az cli
@@ -121,26 +110,26 @@ az extension add --upgrade --yes -n azure-firewall
 #
 # Create Azure Firewall
 #
-az network firewall create -g $RESOURCEGROUP -n arosecure -l $LOCATION
-az network firewall ip-config create -g $RESOURCEGROUP -f arosecure -n fw-config --public-ip-address fw-ip --vnet-name aro-vnet
+az network firewall create -g $NAME -n $NAME -l $LOCATION
+az network firewall ip-config create -g $NAME -f $NAME -n ${NAME}-fw-config --public-ip-address ${NAME}-ip --vnet-name ${NAME}-vnet
 
 #
 # Get Azure Firewall IP's
 #
-FWPUBLIC_IP=$(az network public-ip show -g $RESOURCEGROUP -n fw-ip --query "ipAddress" -o tsv)
-FWPRIVATE_IP=$(az network firewall show -g $RESOURCEGROUP -n arosecure --query "ipConfigurations[0].privateIpAddress" -o tsv)
+FWPUBLIC_IP=$(az network public-ip show -g $NAME -n ${NAME}-ip --query "ipAddress" -o tsv)
+FWPRIVATE_IP=$(az network firewall show -g $NAME -n ${NAME} --query "ipConfigurations[0].privateIpAddress" -o tsv)
 
 #
 # Create route table and routes
 #
-az network route-table create -g $RESOURCEGROUP --name aro-udr
+az network route-table create -g $NAME --name ${NAME}-udr
 
-az network route-table route create -g $RESOURCEGROUP --name aro-udr --route-table-name aro-udr --address-prefix 0.0.0.0/0 --next-hop-type VirtualAppliance --next-hop-ip-address $FWPRIVATE_IP
+az network route-table route create -g $NAME --name ${NAME}-udr --route-table-name ${NAME}-udr --address-prefix 0.0.0.0/0 --next-hop-type VirtualAppliance --next-hop-ip-address $FWPRIVATE_IP
 
 #
 # OpenShift application rules
 #
-az network firewall application-rule create -g $RESOURCEGROUP -f arosecure \
+az network firewall application-rule create -g $NAME -f $NAME \
   --collection-name 'ARO' \
   --action allow \
   --priority 100 \
@@ -152,7 +141,7 @@ az network firewall application-rule create -g $RESOURCEGROUP -f arosecure \
 #
 # Rules to allow images from Docker hub
 #
-az network firewall application-rule create -g $RESOURCEGROUP -f arosecure \
+az network firewall application-rule create -g $NAME -f $NAME \
   --collection-name 'Docker' \
   --action allow \
   --priority 200 \
@@ -164,13 +153,26 @@ az network firewall application-rule create -g $RESOURCEGROUP -f arosecure \
 #
 # Associate ARO subnets to firewall
 #
-az network vnet subnet update -g $RESOURCEGROUP --vnet-name aro-vnet --name master-subnet --route-table aro-udr
-az network vnet subnet update -g $RESOURCEGROUP --vnet-name aro-vnet --name worker-subnet --route-table aro-udr
+az network vnet subnet update -g $NAME --vnet-name ${NAME}-vnet --name master-subnet --route-table ${NAME}-udr
+az network vnet subnet update -g $NAME --vnet-name ${NAME}-vnet --name worker-subnet --route-table ${NAME}-udr
+
+#
+# Create the cluster
+#
+az aro create \
+  --resource-group $NAME \
+  --name $NAME \
+  --vnet ${NAME}-vnet \
+  --master-subnet master-subnet \
+  --worker-subnet worker-subnet \
+  --apiserver-visibility Private \
+  --ingress-visibility Private \
+  --pull-secret @/mnt/c/Users/mtjw/Downloads/pull-secret.txt
 
 #
 # Get public IP address of jumphost
 #
-JUMPHOST_IP=$(az network public-ip show -g arosecure -n jumphost-ip | jq -r '.ipAddress')
+JUMPHOST_IP=$(az network public-ip show -g $NAME -n jumphost-ip | jq -r '.ipAddress')
 
 #
 # Jumphost configuration
@@ -184,11 +186,10 @@ sudo apt install jq -y
 # Connect to ARO
 #
 az login
-CLUSTER=arosecure
-RESOURCEGROUP=arosecure
-ARO_PASSWORD=$(az aro list-credentials -n $CLUSTER -g $RESOURCEGROUP -o json | jq -r '.kubeadminPassword')
-ARO_USERNAME=$(az aro list-credentials -n $CLUSTER -g $RESOURCEGROUP -o json | jq -r '.kubeadminUsername')
-ARO_URL=$(az aro show -n $CLUSTER -g $RESOURCEGROUP -o json | jq -r '.apiserverProfile.url')
+NAME=arosecurefw
+ARO_PASSWORD=$(az aro list-credentials -n $NAME -g $NAME -o json | jq -r '.kubeadminPassword')
+ARO_USERNAME=$(az aro list-credentials -n $NAME -g $NAME -o json | jq -r '.kubeadminUsername')
+ARO_URL=$(az aro show -n $NAME -g $NAME -o json | jq -r '.apiserverProfile.url')
 
 #
 # Install oc cli
@@ -237,17 +238,17 @@ curl microsoft.com
 # seems to fail if the id_rsa.pub public key file exists in the .ssh folder. Fix was
 # to simply move the file out of that folder. 
 #
-# After that though, attempting to establish a tunnel using either the below or sshuttle
-# always resulted in the ssh connection being immediately dropped by the host after
-# authentication. In the end, it was quicker to deploy a Windows VM and access the
-# web console from there!
-#
-CONSOLE_URL=$(az aro show -n $CLUSTER -g $RESOURCEGROUP --query "consoleProfile.url" -o tsv)
-sudo ssh -L 443:$CONSOLE_URL:443 aroadmin@$JUMPHOST_IP
+CONSOLE_URL=$(az aro show -n $NAME -g $NAME --query "consoleProfile.url" -o tsv | sed -e 's/https\?:\/\///' | sed -e 's/\///')
+sudo ssh -N -i /home/mark/.ssh/id_rsa -L 443:$CONSOLE_URL:443 aroadmin@$JUMPHOST_IP
 
 #
-# Example
-# sudo ssh -i /Users/jimzim/.ssh/id_rsa -L 443:console-openshift-console.apps.d5xm5iut.eastus.aroapp.io:443 aroadmin@104.211.18.56
+# Breakdown of the above
 #
-
-https://console-openshift-console.apps.fnew20kz.westeurope.aroapp.io/
+# sudo - we need to map to port 443, so sudo is needed to map to a privileged port
+# ssh - the ssh command
+# -N - don't start an interactive session
+# -i - path to the private key. As we're using sudo, we need to do this otherwise it will try to use root's private key
+# /home/mark/.ssh/id_rsa - actual path of the private key
+# 443 - the port we want to map to locally. Can't use something else, like 8443, because the OpenShift portal will redirect back to 443
+# $CONSOLE_URL - should be the DNS name of the console with https:// and trailing slashes removed
+# aroadmin@JUMPHOST_IP - username and IP address/name of remote host
