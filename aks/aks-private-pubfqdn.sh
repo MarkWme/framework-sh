805760b90e487f6d3de9ab78d13d300f4e36c1c2
Mark Whitby <m@markw.me>
2021-09-16 11:36:57 +0100

    ARO ACR script added

---
+++ aks/aks-private-pubfqdn.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#
+# Private AKS cluster with public FQDN
+#
+location=westeurope
+name=aksprivpub
+virtualNetworkPrefix=10.221.0.0/16
+subnetPrefix=10.221.0.0/24
+jumpboxSubnetPrefix=10.221.100.0/24
+version=$(az aks get-versions -l $location --query "orchestrators[-1].orchestratorVersion" -o tsv)  2>/dev/null
+
+az group create -n $name -l $location
+
+#
+# AKS cluster with Azure CNI
+# Creates VNet, Managed Identity and cluster with three nodes
+#
+az network vnet create -n ${name}-vnet -g $name --address-prefixes $virtualNetworkPrefix -l $location --subnet-name nodepool-subnet --subnet-prefixes $subnetPrefix
+
+az network vnet subnet create -g $name --vnet-name ${name}-vnet --name jumpbox-subnet --address-prefixes $jumpboxSubnetPrefix
+
+vnetId=$(az network vnet subnet list --vnet-name ${name}-vnet --resource-group $name --query "[0].id" -o tsv)
+
+az identity create -n $name -g $name
+identityId=$(az identity show --name $name -g $name --query id -o tsv)
+
+#
+# Create a jumpbox VM
+#
+VMUSERNAME=aksadmin
+
+az vm create --name ubuntu-jump \
+  --resource-group $name \
+  --ssh-key-values ~/.ssh/id_rsa.pub \
+  --admin-username $VMUSERNAME \
+  --image UbuntuLTS \
+  --subnet jumpbox-subnet \
+  --public-ip-address jumphost-ip \
+  --public-ip-sku Standard \
+  --vnet-name ${name}-vnet
+
+
+az aks create \
+    --name $name \
+    --resource-group $name \
+    --kubernetes-version $version \
+    --location $location \
+    --enable-private-cluster \
+    --network-plugin azure \
+    --vnet-subnet-id $vnetId \
+    --docker-bridge-address 172.17.0.1/16 \
+    --dns-service-ip 10.240.0.10 \
+    --service-cidr 10.240.0.0/24 \
+    --enable-managed-identity \
+    --assign-identity $identityId \
+    --node-count 3
+
+
+az aks get-credentials -n $name -g $name --overwrite-existing
+
+#
+# Delete cluster
+#
+az group delete -n $name -y
---
+++ aro/aro-acr.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+#
+# ARO Cluster with custom service principal, resource group and Azure Container Registry
+#
+# Get the pull secret from https://cloud.redhat.com/openshift/install/azure/aro-provisioned
+#
+# Set environment variables
+#
+location=westeurope
+name=arocluster
+virtualNetworkPrefix=10.150.0.0/22
+masterSubnetPrefix=10.150.0.0/23
+workerSubnetPrefix=10.150.2.0/23
+
+#
+# Create resource group
+#
+az group create \
+  --name $name \
+  --location $location
+
+#
+# Create service principal
+#
+clientSecret=$(az ad sp create-for-rbac --name ${name}-spn --skip-assignment --query password --output tsv)
+clientId=$(az ad sp list --display-name ${name}-spn --query '[].appId' -o tsv)
+
+#
+# Create virtual network
+#
+az network vnet create \
+   --resource-group $name \
+   --name ${name}-vnet \
+   --address-prefixes $virtualNetworkPrefix
+
+#
+# Subnet for master nodes
+#
+az network vnet subnet create \
+  --resource-group $name \
+  --vnet-name ${name}-vnet \
+  --name master-subnet \
+  --address-prefixes $masterSubnetPrefix \
+  --service-endpoints Microsoft.ContainerRegistry
+
+#
+# Subnet for worker nodes
+#
+az network vnet subnet create \
+  --resource-group $name \
+  --vnet-name ${name}-vnet \
+  --name worker-subnet \
+  --address-prefixes $workerSubnetPrefix \
+  --service-endpoints Microsoft.ContainerRegistry
+
+#
+# Disable subnet private endpoint policies on the master subnet
+#
+az network vnet subnet update \
+  --name master-subnet \
+  --resource-group $name \
+  --vnet-name ${name}-vnet \
+  --disable-private-link-service-network-policies true
+
+#
+# Create the cluster
+#
+az aro create \
+  --resource-group $name \
+  --cluster-resource-group ${name}-resources \
+  --client-id $clientId \
+  --client-secret $clientSecret \
+  --name $name \
+  --vnet ${name}-vnet \
+  --master-subnet master-subnet \
+  --worker-subnet worker-subnet \
+  --pull-secret @/mnt/c/Users/mtjw/Downloads/pull-secret.txt
+
+#
+# Get the cluster credentials
+#
+az aro list-credentials \
+  --name $name \
+  --resource-group $name
+
+#
+# Get the cluster admin URL
+#
+az aro show \
+    --name $name \
+    --resource-group $name \
+    --query "consoleProfile.url" -o tsv
+
+#
+# Download OpenShift command line tool
+#
+cd ~
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
+sudo tar -zxvf openshift-client-linux.tar.gz -C /usr/local/bin
+
+#
+# Login with oc
+#
+apiServer=$(az aro show -g $name -n $name --query apiserverProfile.url -o tsv)
+userName=$(az aro list-credentials --name $name --resource-group $name | jq -r ".kubeadminUsername")
+password=$(az aro list-credentials --name $name --resource-group $name | jq -r ".kubeadminPassword")
+oc login $apiServer -u $userName -p $password
+
+#
+# Create ACR instance
+#
+az acr create \
+    --name ${name}-acr \
+    --resource-group $name \
+    --sku standard \
+    --admin-enabled true
+
+#
+# Get ACR credentials
+#
+acrUsername=$(az acr credential show -n ${name}-acr | jq -r ".username")
+acrPassword=$(az acr credential show -n ${name}-acr | jq -r ".passwords[0].value")
+
+#
+# Create Kubernetes secret
+# Secret will be created in the current Project (Namespace)
+# and therefore only works with pods running in that Project
+#
+oc create secret docker-registry \
+    --docker-server=${name}-acr.azurecr.io \
+    --docker-username=$acrUsername \
+    --docker-password=$acrPassword \
+    --docker-email=unused \
+    acr-secret
+
+#
+# Delete the cluster
+#
+az aro delete --resource-group $name --name $name
+az group delete --name $name -y
+az ad sp delete --id $clientId
