5302e9f2ebadf355aefa6352221b03eb70e7b6c5
Mark Whitby <m@markw.me>
2022-02-21 12:27:23 +0000

    Supply chain script added

---
+++ supplychain/supplychain.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+# Container Supply Chain 
+#
+# Choose random name for resources
+#
+suffix=$(cat /dev/urandom | tr -dc '[:lower:]' | fold -w ${1:-5} | head -n 1)
+
+location=westeurope
+
+# Set the three registry names, must be globally unique:
+REGISTRY_PUBLIC=publicregistry
+REGISTRY_BASE_ARTIFACTS=contosobaseartifacts
+REGISTRY=contoso
+
+# set the location all resources will be created in:
+RESOURCE_GROUP_LOCATION=eastus
+
+# default resource groups
+REGISTRY_PUBLIC_RG=${REGISTRY_PUBLIC}-rg
+REGISTRY_BASE_ARTIFACTS_RG=${REGISTRY_BASE_ARTIFACTS}-rg
+REGISTRY_RG=${REGISTRY}-rg
+
+# fully qualified registry urls
+REGISTRY_DOCKERHUB_URL=docker.io
+REGISTRY_PUBLIC_URL=${REGISTRY_PUBLIC}.azurecr.io
+REGISTRY_BASE_ARTIFACTS_URL=${REGISTRY_BASE_ARTIFACTS}.azurecr.io
+REGISTRY_URL=${REGISTRY}.azurecr.io
+
+# Azure key vault for storing secrets, name must be globally unique
+AKV=acr-task-credentials
+AKV_RG=${AKV}-rg
+
+# ACI for hosting the deployed application
+ACI=hello-world-aci
+ACI_RG=${ACI}-rg
+
+read -p "Enter Docker PAT?" dockerPAT
