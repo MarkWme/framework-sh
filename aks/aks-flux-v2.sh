a46ed99d4d81a5ee1b81c46885d6cf077399c940
Mark Whitby <m@markw.me>
2022-03-02 09:08:24 +0000

    Update flux script

--- aks/aks-flux-v2.sh
+++ aks/aks-flux-v2.sh
@@ -5,8 +5,15 @@
 #
 location=westeurope
 name=aks-flux
-virtualNetworkPrefix=10.25.0.0/16
-subnetPrefix=10.25.0.0/24
+#
+# Calculate next available network address space
+#
+number=0
+number=$(az network vnet list --query "[].addressSpace.addressPrefixes" -o tsv | cut -d . -f 2 | sort | tail -n 1)
+networkNumber=$(expr $number + 1)
+virtualNetworkPrefix=10.${networkNumber}.0.0/16
+aksSubnetPrefix=10.${networkNumber}.0.0/24
+
 version=$(az aks get-versions -l $location --query "orchestrators[-1].orchestratorVersion" -o tsv)  2>/dev/null
 
 az group create -n $name -l $location
@@ -15,7 +22,7 @@ az group create -n $name -l $location
 # AKS cluster with Azure CNI
 # Creates VNet, Managed Identity and cluster with three nodes
 #
-az network vnet create -n  ${name}-vnet -g $name --address-prefixes $virtualNetworkPrefix -l $location --subnet-name  ${name}-subnet  --subnet-prefixes $subnetPrefix
+az network vnet create -n  ${name}-vnet -g $name --address-prefixes $virtualNetworkPrefix -l $location --subnet-name  ${name}-subnet  --subnet-prefixes $aksSubnetPrefix
 vnetId=$(az network vnet subnet list --vnet-name ${name}-vnet --resource-group $name --query "[?name=='${name}-subnet'].id" -o tsv)
 
 az identity create -n $name -g $name
@@ -44,14 +51,36 @@ az feature register --namespace Microsoft.ContainerService --name AKS-ExtensionM
 az feature register --namespace Microsoft.KubernetesConfiguration --name fluxConfigurations
 az provider register --namespace Microsoft.Kubernetes
 az provider register --namespace Microsoft.KubernetesConfiguration
+az provider register --namespace Microsoft.ContainerService
 
-az extension add --source ./k8s_configuration-1.1.0b1-py3-none-any.whl --yes
-az extension add --source ./k8s_extension_private-0.7.1b1-py3-none-any.whl --yes
+az extension add -n k8s-configuration
+az extension add -n k8s-extension
+
+# az extension add --source ./k8s_configuration-1.1.0b1-py3-none-any.whl --yes
+# az extension add --source ./k8s_extension_private-0.7.1b1-py3-none-any.whl --yes
+
+#az k8s-configuration flux create \
+#    -g $name -c $name -t managedClusters \
+#    -n gitops-demo --namespace gitops-demo --scope cluster \
+#    -u https://github.com/fluxcd/flux2-kustomize-helm-example --branch main --kustomization name=kustomization1 prune=true
+
 
 az k8s-configuration flux create \
-    -g $name -c $name -t managedClusters \
-    -n gitops-demo --namespace gitops-demo --scope cluster \
-    -u https://github.com/Azure/arc-k8s-demo --branch main --kustomization name=kustomization1 prune=true
+    -g $name \
+    -c $name \
+    -n gitops-demo \
+    --namespace gitops-demo \
+    -t managedClusters \
+    --scope cluster \
+    -u https://github.com/fluxcd/flux2-kustomize-helm-example \
+    --branch main \
+    --kustomization name=infra path=./infrastructure prune=true \
+    --kustomization name=apps path=./apps/staging prune=true dependsOn=\["infra"\]
+
+#
+# Show the configuration
+#
+az k8s-configuration flux show -g $name -c $name -n gitops-demo -t managedClusters
 
 
 #
--- supplychain/supplychain.sh
+++ supplychain/supplychain.sh
@@ -5,17 +5,26 @@
 #
 # Choose random name for resources
 #
+# Pre-req's
+#
+# Fork these repos
+# https://github.com/importing-public-content/base-image-node.git
+# https://github.com/importing-public-content/import-baseimage-node.git
+# https://github.com/importing-public-content/hello-world.git
+#
+# Create a GitHub PAT
+# Create a Docker PAT
+
+export LC_CTYPE=C
 suffix=$(cat /dev/urandom | tr -dc '[:lower:]' | fold -w ${1:-5} | head -n 1)
 
-location=westeurope
+
+RESOURCE_GROUP_LOCATION=westeurope
 
 # Set the three registry names, must be globally unique:
-REGISTRY_PUBLIC=publicregistry
-REGISTRY_BASE_ARTIFACTS=contosobaseartifacts
-REGISTRY=contoso
-
-# set the location all resources will be created in:
-RESOURCE_GROUP_LOCATION=eastus
+REGISTRY_PUBLIC=publicregistry${suffix}
+REGISTRY_BASE_ARTIFACTS=contosobaseartifacts${suffix}
+REGISTRY=contoso${suffix}
 
 # default resource groups
 REGISTRY_PUBLIC_RG=${REGISTRY_PUBLIC}-rg
@@ -29,11 +38,202 @@ REGISTRY_BASE_ARTIFACTS_URL=${REGISTRY_BASE_ARTIFACTS}.azurecr.io
 REGISTRY_URL=${REGISTRY}.azurecr.io
 
 # Azure key vault for storing secrets, name must be globally unique
-AKV=acr-task-credentials
+AKV=acr-task-creds-${suffix}
 AKV_RG=${AKV}-rg
 
 # ACI for hosting the deployed application
-ACI=hello-world-aci
+ACI=hello-world-aci-${suffix}
 ACI_RG=${ACI}-rg
 
-read -p "Enter Docker PAT?" dockerPAT
+GIT_BASE_IMAGE_NODE=https://github.com/markwme/base-image-node.git#main
+GIT_NODE_IMPORT=https://github.com/markwme/import-baseimage-node.git#main
+GIT_HELLO_WORLD=https://github.com/markwme/hello-world.git#main
+
+#
+# Get access tokens for GitHub and Docker
+#
+read -p "Enter GitHub PAT" GIT_TOKEN
+read -p "Enter Docker ID" REGISTRY_DOCKERHUB_USER
+read -p "Enter Docker PAT?" REGISTRY_DOCKERHUB_PASSWD
+
+#
+# Create three ACR instances
+#
+az group create --name $REGISTRY_PUBLIC_RG --location $RESOURCE_GROUP_LOCATION
+az acr create --resource-group $REGISTRY_PUBLIC_RG --name $REGISTRY_PUBLIC --sku Premium
+
+az group create --name $REGISTRY_BASE_ARTIFACTS_RG --location $RESOURCE_GROUP_LOCATION
+az acr create --resource-group $REGISTRY_BASE_ARTIFACTS_RG --name $REGISTRY_BASE_ARTIFACTS --sku Premium
+
+az group create --name $REGISTRY_RG --location $RESOURCE_GROUP_LOCATION
+az acr create --resource-group $REGISTRY_RG --name $REGISTRY --sku Premium
+
+#
+# Create Key Vault instance to store access tokens
+#
+az group create --name $AKV_RG --location $RESOURCE_GROUP_LOCATION
+az keyvault create --resource-group $AKV_RG --name $AKV
+
+az keyvault secret set \
+--vault-name $AKV \
+--name registry-dockerhub-user \
+--value $REGISTRY_DOCKERHUB_USER
+
+az keyvault secret set \
+--vault-name $AKV \
+--name registry-dockerhub-password \
+--value $REGISTRY_DOCKERHUB_PASSWD
+
+az keyvault secret set --vault-name $AKV --name github-token --value $GIT_TOKEN
+
+az keyvault secret show --vault-name $AKV --name github-token --query value -o tsv
+
+#
+# Create resource group for Azure Container Instance
+#
+
+az group create --name $ACI_RG --location $RESOURCE_GROUP_LOCATION
+
+#
+# Create the simulated Node.js public base image
+#
+# Done this way rather than using the real base image so that we can make changes to it
+# and simulate what happens when a real base image is updated
+#
+# Setup the task
+#
+az acr task create \
+  --name node-public \
+  -r $REGISTRY_PUBLIC \
+  -f acr-task.yaml \
+  --context $GIT_BASE_IMAGE_NODE \
+  --git-access-token $(az keyvault secret show \
+                        --vault-name $AKV \
+                        --name github-token \
+                        --query value -o tsv) \
+  --set REGISTRY_FROM_URL=${REGISTRY_DOCKERHUB_URL}/ \
+  --assign-identity
+
+#
+# Add Docker hub credentials to the task
+#
+az acr task credential add \
+  -n node-public \
+  -r $REGISTRY_PUBLIC \
+  --login-server $REGISTRY_DOCKERHUB_URL \
+  -u https://${AKV}.vault.azure.net/secrets/registry-dockerhub-user \
+  -p https://${AKV}.vault.azure.net/secrets/registry-dockerhub-password \
+  --use-identity '[system]'
+
+#
+# Grant the task access to Key Vault
+#
+az keyvault set-policy \
+  --name $AKV \
+  --resource-group $AKV_RG \
+  --object-id $(az acr task show \
+                  --name node-public \
+                  --registry $REGISTRY_PUBLIC \
+                  --query identity.principalId --output tsv) \
+  --secret-permissions get
+
+#
+# Run the task manually
+#
+az acr task run -r $REGISTRY_PUBLIC -n node-public
+
+#
+# Confirm the new image is available
+#
+az acr repository show-tags -n $REGISTRY_PUBLIC --repository node
+
+#
+# Create the hello-world image
+#
+# Builds an app container based on the node image created above
+#
+# Create a token to allow access to the simulated public repo
+#
+az keyvault secret set \
+  --vault-name $AKV \
+  --name "registry-${REGISTRY_PUBLIC}-user" \
+  --value "registry-${REGISTRY_PUBLIC}-user"
+
+az keyvault secret set \
+  --vault-name $AKV \
+  --name "registry-${REGISTRY_PUBLIC}-password" \
+  --value $(az acr token create \
+              --name "registry-${REGISTRY_PUBLIC}-user" \
+              --registry $REGISTRY_PUBLIC \
+              --scope-map _repositories_pull \
+              -o tsv \
+              --query 'credentials.passwords[0].value')
+#
+# Create token to allow ACI to have pull access to the private registry
+#
+az keyvault secret set \
+  --vault-name $AKV \
+  --name "registry-${REGISTRY}-user" \
+  --value "registry-${REGISTRY}-user"
+
+az keyvault secret set \
+  --vault-name $AKV \
+  --name "registry-${REGISTRY}-password" \
+  --value $(az acr token create \
+              --name "registry-${REGISTRY}-user" \
+              --registry $REGISTRY \
+              --repository hello-world content/read \
+              -o tsv \
+              --query 'credentials.passwords[0].value')
+#
+# Create task to build image
+#
+az acr task create \
+  -n hello-world \
+  -r $REGISTRY \
+  -f acr-task.yaml \
+  --context $GIT_HELLO_WORLD \
+  --git-access-token $(az keyvault secret show \
+                        --vault-name $AKV \
+                        --name github-token \
+                        --query value -o tsv) \
+  --set REGISTRY_FROM_URL=${REGISTRY_PUBLIC_URL}/ \
+  --set KEYVAULT=$AKV \
+  --set ACI=$ACI \
+  --set ACI_RG=$ACI_RG \
+  --assign-identity
+
+az acr task credential update \
+  -n hello-world \
+  -r $REGISTRY \
+  --login-server $REGISTRY_PUBLIC_URL \
+  -u https://acr-task-creds-geskk.vault.azure.net/secrets/registry-publicregistrygeskk-user \
+  -p https://acr-task-creds-geskk.vault.azure.net/secrets/registry-publicregistrygeskk-password \
+  --use-identity '[system]'
+
+az keyvault set-policy \
+  --name $AKV \
+  --resource-group $AKV_RG \
+  --object-id $(az acr task show \
+                  --name hello-world \
+                  --registry $REGISTRY \
+                  --query identity.principalId --output tsv) \
+  --secret-permissions get
+
+az role assignment create \
+  --assignee $(az acr task show \
+  --name hello-world \
+  --registry $REGISTRY \
+  --query identity.principalId --output tsv) \
+  --scope $(az group show -n $ACI_RG --query id -o tsv) \
+  --role owner
+
+az acr task run -r $REGISTRY -n hello-world
+
+az container show \
+  --resource-group $ACI_RG \
+  --name ${ACI} \
+  --query ipAddress.ip \
+  --out tsv
+
+  
